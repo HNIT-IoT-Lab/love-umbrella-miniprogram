6d5cb72ff6690260133b5838506bf4a826fe0200
何福任 <1740610992@qq.com>
2021-12-19 22:31:28 +0800

    用户绑定手机号页面的初步编写

--- pages/telform/telform.js
+++ pages/telform/telform.js
@@ -5,6 +5,10 @@ Page({
      * 页面的初始数据
      */
     data: {
+        state: String,
+        toastMessage: String,
+        hasSendCode: false,
+        countDownNum: 60
 
     },
 
@@ -66,5 +70,60 @@ Page({
 
     bindtel() {
         console.log('绑定手机号')
+    },
+
+    closeToast() {
+        setTimeout(()=>{
+            this.setData({
+                state: null
+            })
+        },1500)
+    },
+
+    formInputChange(evt) {
+        console.log(evt.detail.value)
+    },
+
+    bindtel() {
+        console.log('绑定手机号')
+        this.setData({
+            state: "success",
+            toastMessage: "绑定成功"
+        })
+        this.closeToast()
+        // 跳转到首页
+        // wx.navigateBack({
+        //   delta: 2
+        // })
+    },
+
+    sendCode(evt) {
+        console.log(evt)
+        this.setData({
+            hasSendCode:true
+        })
+        this.waitForInputCode()
+    },
+
+    waitForInputCode() {
+        console.log('验证码已发送，等待输入')
+        var _this = this;
+        var countDownNum = _this.data.countDownNum;             // 获取倒计时初始值
+        var timer = setInterval(function() {
+        countDownNum -= 1;
+        _this.setData({
+            countDownNum: countDownNum
+        })
+        if(countDownNum <= -1) {
+            //取消setInterval函数
+            clearInterval(timer);
+            // 重置数据，便于后续使用
+            _this.setData({
+                countDownNum: 60,
+                hasSendCode: false
+            })
+
+        }
+        }, 1000)
     }
 })
